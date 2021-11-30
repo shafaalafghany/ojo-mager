7f489a7e4f59c93a54c1d30daa7de7f996753716
Shafa Alafghany <alafghanyshafa@gmail.com>
2021-11-30 07:03:01 +0700

    add register and login screens

--- app/src/main/java/com/kelompok1/ojomager/LoginActivity.kt
+++ app/src/main/java/com/kelompok1/ojomager/LoginActivity.kt
@@ -3,16 +3,18 @@ package com.kelompok1.ojomager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        val btnSwitch = findViewById<Button>(R.id.btn_switch)
+        supportActionBar?.hide()
 
-        btnSwitch.setOnClickListener {
+        val btnCreateAccount = findViewById<TextView>(R.id.tv_create_account)
+
+        btnCreateAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
